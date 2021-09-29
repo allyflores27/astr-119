72eeedc578f6cb39cfb02c87c42322a044307de4
allyflores27 <65577122+allyflores27@users.noreply.github.com>
2021-09-29 15:36:01 -0700

    Add files via upload

---
+++ first-homework.py
@@ -0,0 +1,6 @@
+# This program is the first homework for Astro 119 Fall 2021.
+# This program is meant to print my name and preferred pronouns.
+
+
+print("Hello there! My name is Allyson Flores and"
+	+" my preferred pronouns are she/her/they/them.")
